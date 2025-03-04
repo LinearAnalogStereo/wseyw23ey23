f2ee598b34acc857cd6df5d8662394db24c88a01
LinearAnalogStereo <99267279+LinearAnalogStereo@users.noreply.github.com>
2025-03-04 17:20:00 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,16 @@
 tor --runasdaemon 1 --log notice file:/var/log/tor/log &
 TOR_PID=$!
 
+# Ждем, пока Tor создаст файл с onion-адресом
+while [ ! -f /usr/local/var/lib/tor/hidden_service/hostname ]; do
+    echo "Ожидание создания onion-адреса..."
+    sleep 1
+done
+
+# Выводим onion-адрес
+ONION_ADDRESS=$(cat /usr/local/var/lib/tor/hidden_service/hostname)
+echo "Onion-адрес: $ONION_ADDRESS"
+
 # Запуск Nginx
 nginx -g "daemon off;" &
 NGINX_PID=$!
